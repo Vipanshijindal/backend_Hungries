32a00001dc43dc7544eb926d9deea17416b8c95f
vipanshijindal <vipanshijindal008@gmail.com>
2024-04-16 20:31:26 +0530

    updated files

--- controllers/firmController.js
+++ controllers/firmController.js
@@ -1,6 +1,6 @@
 const Vendor = require("../models/Vendor");
 const multer = require("multer");
-const Firm = require("../models/firm");
+const Firm = require("../models/Firm");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/"); //Destination folder where the uploaded images will be stored
