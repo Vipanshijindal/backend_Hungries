8a1d2fbe6555b049e5c38a8fbd0ca1b7fa6ac6a4
vipanshijindal <vipanshijindal008@gmail.com>
2024-04-19 02:44:50 +0530

    updated

--- models/Firm.js
+++ models/Firm.js
@@ -1,48 +1,44 @@
-const mongoose = require("mongoose");
-const Firmschema = new mongoose.Schema({
-  firmName: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  area: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: [
-      {
+const mongoose = require('mongoose');
+
+const firmSchema = new mongoose.Schema({
+    firmName: {
         type: String,
-        enum: ["veg", "non-veg"],
-      },
-    ],
-  },
-  region: {
-    type: [
-      {
+        required: true,
+        unique: true
+    },
+    area: {
         type: String,
-        enum: ["south-indian", "north-indian", "chinese", "bakery"],
-      },
-    ],
-  },
-  offer: {
-    type: String,
-  },
-  image: {
-    type:String
-  },
-  vendor:[
-    {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Vendor'
-    }
-  ],
- product:[
-    {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Product'
-    }
-  ]
+        required: true,
+    },
+    category: {
+        type: [{
+            type: String,
+            enum: ['veg', 'non-veg']
+        }]
+    },
+    region: {
+        type: [{
+            type: String,
+            enum: ['south-indian', 'north-indian', 'chinese', 'bakery']
+        }]
+    },
+    offer: {
+        type: String,
+
+    },
+    image: {
+        type: String
+    },
+    vendor: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Vendor'
+    }],
+    products: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }]
 });
-const firm = mongoose.model("firm", Firmschema);
-module.exports = firm;
+
+const Firm = mongoose.model('Firm', firmSchema);
+
+module.exports = Firm
--- models/Product.js
+++ models/Product.js
@@ -1,39 +1,35 @@
-const mongoose = require("mongoose");
-const productSchema = new mongoose.Schema({
-  productName: {
-    type: String,
-    required: true,
-  },
- price: {
-    type: String,
-    required: true,
-   
-  },
-  category: {
-    type: [
-      {
-        type: String,
-        enum: ["veg", "non-veg"],
-      },
-    ],
-  },
-  image:{
-    type:String
-  },
-  bestSeller:{
-    type:Boolean
-  },
-  description:{
-    type:String
-  },
+const mongoose = require('mongoose');
 
- 
-  firm: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "firm",
+const productSchema = new mongoose.Schema({
+    productName: {
+        type: String,
+        required: true
     },
-  ],
+    price: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: [{
+            type: String,
+            enum: ['veg', 'non-veg']
+        }]
+    },
+    image: {
+        type: String
+    },
+    bestSeller: {
+        type: Boolean
+    },
+    description: {
+        type: String
+    },
+    firm: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Firm'
+    }]
 });
-const Product = mongoose.model("Product", productSchema);
-module.exports = Product;
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product
